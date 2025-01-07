4297c61b55411b5fae282b0eb06c81d54bdb2d7e
karan2782 <karantanwar5005@gmail.com>
2025-01-07 12:17:02 +0530

    added margin

--- src/page/FooterPage.jsx
+++ src/page/FooterPage.jsx
@@ -6,7 +6,7 @@ function FooterPage() {
   return (
     <div className='py-[70px] px-[54px] bg-[#2F1893] text-[#ffffff]' >
       <UpperFooter />
-      <div className='h-[2px] border-2 border-[#ffffff]' ></div>
+      <div className='h-[2px] border-2 border-[#ffffff] my-[40px]'  ></div>
       <LowerFooter />
     </div>
   )
