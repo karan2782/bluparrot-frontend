777e7a9e1b33f9b02bbc92c936937aa85b132684
karan2782 <karantanwar5005@gmail.com>
2025-01-07 11:23:55 +0530

    2nd

--- src/component/LowerFooter.jsx
+++ src/component/LowerFooter.jsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 function LowerFooter() {
   return (
-    <div>
-      
+    <div className='flex justify-between'>
+      <div className='flex gap-4'>
+        <p>Tour</p>
+        <p>Features</p>
+        <p>Pricing Plans</p>
+        <p>Our Works</p>
+        <p>Contacts</p>
+      </div>
+
+      <div>
+      © 2017 Designmodo. All rights reserved.
+      </div>
     </div>
   )
 }
