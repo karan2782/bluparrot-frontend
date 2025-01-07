20fe2d889e52f539b02feed273462fb5104d5ea6
karan2782 <karantanwar5005@gmail.com>
2025-01-07 12:21:44 +0530

    responsiveness

--- src/component/UpperFooter.jsx
+++ src/component/UpperFooter.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
-import { TiSocialFacebook, TiSocialTwitter } from "react-icons/ti"
+import React from "react";
+import { TiSocialFacebook, TiSocialTwitter } from "react-icons/ti";
 import { FaGooglePlusG } from "react-icons/fa";
 
 function UpperFooter() {
   return (
     <div>
+      <div className="flex flex-col md:flex-row justify-between items-center gap-4">
+        {/* Brand Name */}
+        <h1 className="font-[700] text-[24px]">Startup 3</h1>
 
-      <div className="flex justify-between">
-
-        <h1 className='font-[700] text-[24px]'>Startup 3</h1>
-        <div className='flex gap-x-[28px]'>
-            <p>Privacy Policy</p>
-            <p>Terms</p>
-            <p className="text-[18px]"> <TiSocialFacebook /> </p>
-            <p className="text-[18px]" ><TiSocialTwitter /></p>
-            <p className="text-[18px]"><FaGooglePlusG /> </p>
+        {/* Links and Icons */}
+        <div className="flex flex-col md:flex-row items-center gap-4 md:gap-6">
+          <p>Privacy Policy</p>
+          <p>Terms</p>
+          <p className="text-[18px]">
+            <TiSocialFacebook />
+          </p>
+          <p className="text-[18px]">
+            <TiSocialTwitter />
+          </p>
+          <p className="text-[18px]">
+            <FaGooglePlusG />
+          </p>
         </div>
-            
       </div>
-
-
     </div>
-  )
+  );
 }
 
-export default UpperFooter
+export default UpperFooter;
