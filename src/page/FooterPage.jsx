41a314eac0be8e6cc614cf5dc59b751b05db4487
karan2782 <karantanwar5005@gmail.com>
2025-01-07 12:13:11 +0530

    adding style

--- src/page/FooterPage.jsx
+++ src/page/FooterPage.jsx
@@ -4,9 +4,9 @@ import LowerFooter from '../component/LowerFooter'
 
 function FooterPage() {
   return (
-    <div className='py-[70px] px-[54px]' >
+    <div className='py-[70px] px-[54px] bg-[#2F1893] text-[#ffffff]' >
       <UpperFooter />
-      <div></div>
+      <div className='h-[2px] border-2 border-[#ffffff]' ></div>
       <LowerFooter />
     </div>
   )
