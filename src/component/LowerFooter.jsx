c792df2fc5ec80996fab9909d0f803fc9c802a5e
karan2782 <karantanwar5005@gmail.com>
2025-01-07 12:24:10 +0530

    lower footer

--- src/component/LowerFooter.jsx
+++ src/component/LowerFooter.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
 function LowerFooter() {
   return (
-    <div className='flex justify-between'>
-      <div className='flex gap-4'>
+    <div className="flex flex-col md:flex-row justify-between items-center gap-4 md:gap-0">
+      {/* Navigation Links */}
+      <div className="flex flex-wrap justify-center md:justify-start gap-4">
         <p>Tour</p>
         <p>Features</p>
         <p>Pricing Plans</p>
@@ -11,11 +12,12 @@ function LowerFooter() {
         <p>Contacts</p>
       </div>
 
-      <div>
-      © 2017 Designmodo. All rights reserved.
+      {/* Copyright Information */}
+      <div className="text-center md:text-left">
+        © 2017 Designmodo. All rights reserved.
       </div>
     </div>
-  )
+  );
 }
 
-export default LowerFooter
+export default LowerFooter;
